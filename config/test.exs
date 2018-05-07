082d195af433b46046895fd503176906c65b45d0
Guillaume Bailleul <laibulle@gmail.com>
2018-05-08 01:50:56 +0200

    fix postgres

--- config/test.exs
+++ config/test.exs
@@ -13,7 +13,7 @@ config :logger, level: :warn
 config :hibou, Hibou.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: System.get_env("PGPASSWORD") || "postgres",
   database: "postgres",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox,
