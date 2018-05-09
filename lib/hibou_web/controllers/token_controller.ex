8998833402582c0041316b83a75f8b5b6d2de4ce
Guillaume Bailleul <laibulle@gmail.com>
2018-05-10 01:31:53 +0200

    move controller logix

--- lib/hibou_web/controllers/token_controller.ex
+++ lib/hibou_web/controllers/token_controller.ex
@@ -1,11 +1,7 @@
 defmodule HibouWeb.TokenController do
   use HibouWeb, :controller
 
-  alias Hibou.Model.User
-  alias Hibou.Storage
-
-  @invalid_credentials %{"message" => "invalid credentials"}
-  @invalid_request %{"message" => "invalid credentials"}
+  alias Hibou.OAuth2
 
   defp render_json(conn, code, data) do
     conn
@@ -14,42 +10,9 @@ defmodule HibouWeb.TokenController do
   end
 
   def create(conn, %{"grant_type" => grant_type} = params) do
-    case grant_type do
-      "authorization_code" ->
-        case Storage.get_authorization_by_code(params["code"]) do
-          nil ->
-            conn |> render_json(400, @invalid_request)
-
-          auth ->
-            {:ok, access_token, _claims} = Hibou.Guardian.encode_and_sign(auth.user, %{})
-            {:ok, refresh_token, _claims} = Hibou.Guardian.encode_and_sign(auth.user, %{})
-
-            conn
-            |> render_json(200, %{
-              "access_token" => access_token,
-              "refresh_token" => refresh_token,
-              "expires_in" => 3600
-            })
-        end
-
-      "password" ->
-        case Storage.get_user_by_username(params["username"]) do
-          nil ->
-            conn |> render_json(400, @invalid_credentials)
-
-          user ->
-            User.check_password(params["password"], user.password_hash)
-
-            {:ok, access_token, _claims} = Hibou.Guardian.encode_and_sign(user, %{})
-            {:ok, refresh_token, _claims} = Hibou.Guardian.encode_and_sign(user, %{})
-
-            conn
-            |> render_json(200, %{
-              "access_token" => access_token,
-              "refresh_token" => refresh_token,
-              "expires_in" => 3600
-            })
-        end
+    case OAuth2.authorize(params) do
+      {:error, data} -> conn |> render_json(400, data)
+      {:ok, data} -> conn |> render_json(200, data)
     end
   end
 end
