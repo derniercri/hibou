47b553bc0436d960cdb8c8020cf97d9ef47ed198
Guillaume Bailleul <laibulle@gmail.com>
2018-05-27 19:59:32 +0200

    add model configuration

--- lib/hibou/storage_ecto.ex
+++ lib/hibou/storage_ecto.ex
@@ -3,18 +3,16 @@ defmodule Hibou.StorageEcto do
 
   import Ecto.Query, warn: false
 
-  def get_user_by_id!(id), do: repo().get!(Config.user_model(), id)
+  def get_user_by_id!(id), do: repo().get!(user_model(), id)
 
   def get_user_by_username(username),
     do:
-      repo().one(
-        from(u in Config.user_model(), where: u.email == ^username or u.username == ^username)
-      )
+      repo().one(from(u in user_model(), where: u.email == ^username or u.username == ^username))
 
-  def get_client_by_id(id), do: repo().one(from(c in Config.client_model(), where: c.id == ^id))
+  def get_client_by_id(id), do: repo().one(from(c in client_model(), where: c.id == ^id))
 
   def get_authorization_by_code(code),
     do:
-      repo().one(from(a in Config.authorization_model(), where: a.code == ^code))
+      repo().one(from(a in authorization_model(), where: a.code == ^code))
       |> repo().preload([:user, :client])
 end
