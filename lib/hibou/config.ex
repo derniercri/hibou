15f1e3fabe3554ac4b8e2d22623b988a9dc47c16
Guillaume Bailleul <laibulle@gmail.com>
2018-05-27 19:58:27 +0200

    add model configuration

--- lib/hibou/config.ex
+++ lib/hibou/config.ex
@@ -13,11 +13,11 @@ defmodule Hibou.Config do
   def storage, do: Application.get_env(:hibou, :storage)
 
   @spec user_model() :: Hibou.Model.User
-  def user_model(), do: Application.get_env(:hibou, :user_model)
+  def user_model, do: Application.get_env(:hibou, :user_model)
 
   @spec client_model() :: Hibou.Model.Client
-  def client_model(), do: Application.get_env(:hibou, :client_model)
+  def client_model, do: Application.get_env(:hibou, :client_model)
 
   @spec authorization_model() :: Hibou.Model.Authorization
-  def authorization_model(), do: Application.get_env(:hibou, :authorization_model)
+  def authorization_model, do: Application.get_env(:hibou, :authorization_model)
 end
